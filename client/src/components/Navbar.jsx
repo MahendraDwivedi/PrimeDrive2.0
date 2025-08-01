e7abc267b5fba2bba2eb9b83589ff52fb6618ea5
MahendraDwivedi <dwivedimahendra9876@gmail.com>
2025-08-01 11:38:09 -0700

    motion effect added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { assets, menuLinks } from '../assets/assets'
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useAppContext } from '../context/AppContext';
+import {motion} from 'motion/react'
 
 const Navbar = () => { 
 
@@ -28,9 +29,13 @@ const Navbar = () => {
     }
     
     return (
-        <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all ${location.pathname === "/" ? "bg-light":"bg-white"}`}>
+        <motion.div 
+        initial={{y: -20, opacity:0}}
+        animate={{y:0,opacity:1}}
+        transition={{duration:0.5}}
+        className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all ${location.pathname === "/" ? "bg-light":"bg-white"}`}>
             <Link to="/">
-                <img src={assets.logo} alt="logo" className='h-8' />
+                <motion.img whileHover={{scale:1.05}} src={assets.logo} alt="logo" className='h-8' />
             </Link>
 
             <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === "/" ? "bg-light":"bg-white"} ${open ? "max-sm:translate-x-0":"max-sm:translate-x-full"}`}>
@@ -54,7 +59,7 @@ const Navbar = () => {
             <button className='sm:hidden cursor-pointer' aria-label="Menu" onClick={()=>setOpen(!open)}>
                 <img src={open ? assets.close_icon : assets.menu_icon} alt="" />
             </button>
-        </div>
+        </motion.div>
     )
 }
 
